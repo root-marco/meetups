358df2b4b47a83d550d378b5c80afccc381c480c
slowed <rootslowed@gmail.com>
2021-06-25 17:31:56 -0300

    Meetup model first commit

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
