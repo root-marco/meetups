01a06f3867ab04e885016d17de15137b6c394ad7
slowed <rootslowed@gmail.com>
2021-06-27 09:22:06 -0300

    admin area configs

--- meetups/admin.py
+++ meetups/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Meetup
 
-admin.site.register(Meetup)
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_filter = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Meetup, MeetupAdmin)
