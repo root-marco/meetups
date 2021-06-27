6d7386e9716c3af322048e16d4853268f691431c
slowed <rootslowed@gmail.com>
2021-06-27 09:22:37 -0300

    image field added

--- meetups/models.py
+++ meetups/models.py
@@ -5,3 +5,4 @@ class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    image = models.ImageField(upload_to='images')
