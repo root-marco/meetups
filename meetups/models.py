a4394508c719fd310ca2c1c959b995bc5f5b98f3
slowed <rootslowed@gmail.com>
2021-06-27 09:41:02 -0300

    location model implemented

--- meetups/models.py
+++ meetups/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
