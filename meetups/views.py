a4daa6a03bae5320a4f9d309386cc908cf490c85
slowed <rootslowed@gmail.com>
2021-07-03 12:31:40 -0300

    views index, meetup details and confirm registration

--- meetups/views.py
+++ meetups/views.py
@@ -1,25 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Meetup
+from .models import Meetup, Participant
+from .forms import RegistrationForm
+
 
 def index(request):
-    
     meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'meetups': meetups,
+        'meetups': meetups
     })
 
 
-def meetup_detail(request, meetup_slug):
-
+def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        return render(request, 'meetups/meetup-detail.html', {
-            'meetup_found': True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description': selected_meetup.description,
-        })
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                user_email = registration_form.cleaned_data['email']
+                participant, _ = Participant.objects.get_or_create(email=user_email)
+                selected_meetup.participants.add(participant)
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
+
+        return render(request, 'meetups/meetup-details.html', {
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as exc:
-        return render(request, 'meetups/meetup-detail.html', {
-            'meetup_found': False,
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
         })
+
+
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
