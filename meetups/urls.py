1b9a4ad138e284fa1dba9712008b423b2a59c9c9
slowed <rootslowed@gmail.com>
2021-07-03 12:31:12 -0300

    urls done

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:meetup_slug>', views.meetup_detail, name='detail')
+  path('', views.index, name='all-meetups'),
+  path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+  path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
 ]
